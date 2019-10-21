b13c439aaff4a5e79e57d2723be8b61fac462cc9
bcaller <bcaller@users.noreply.github.com>
2019-10-22 00:21:36 +0100

    Short flag zip: Update solution

--- short-flag-zip/solution.py
+++ short-flag-zip/solution.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import string
 import zipfile
 import zlib
@@ -8,16 +9,22 @@ from itertools import product
 # Since the flag file is short, and the CRC is of the unencrypted file, we can brute force the flag
 
 
-crc_prefix = zlib.crc32(b"flag{")
-alphabet = string.ascii_letters + string.digits
+def crack():
+    crc_prefix = zlib.crc32(b"flag{")
+    alphabet = string.ascii_letters + string.digits
+
+    with zipfile.ZipFile("short-flag.zip") as z:
+        flag_info = z.getinfo("flag.txt")
+        crc = flag_info.CRC
+        print("CRC", crc)
+        n_guess = flag_info.file_size - len("flag{}")
+        print("Guess length", n_guess)
+        for content in product(alphabet, repeat=n_guess):
+            attempt = ("".join(content) + "}").encode()
+            if zlib.crc32(attempt, crc_prefix) == crc:
+                print(b"flag{" + attempt)
+                return
+    raise Exception("FAIL")
 
 
-with zipfile.ZipFile("short-flag.zip") as z:
-    flag_info = z.getinfo("flag.txt")
-    crc = flag_info.CRC
-    n_guess = flag_info.file_size - len("flag{}")
-    for content in product(alphabet, repeat=n_guess):
-        attempt = ("".join(content) + "}").encode()
-        if zlib.crc32(attempt, crc_prefix) == crc:
-            print(b"flag{" + attempt)
-            break
+crack()
