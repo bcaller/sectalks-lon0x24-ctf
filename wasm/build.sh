aba60983e1fb29b1f6c4fa555993d1db25643544
bcaller <bcaller@users.noreply.github.com>
2019-10-23 01:33:08 +0100

    Fix wasm build

--- wasm/build.sh
+++ wasm/build.sh
@@ -4,3 +4,9 @@ docker build . -t wasmy && \
   docker run --rm -v "$(pwd):/src:ro" -v "$(pwd)/out:/out" wasmy \
     /bin/ash -c \
       "/wabt-bin/wat2wasm /src/hi.wat -o /out/flag.wasm && /check.js /out/flag.wasm 'flag{assemble_oozy_haze}'"
+
+(
+cd out;
+zip wasm.zip flag.wasm index.html;
+mv wasm.zip ../;
+)
