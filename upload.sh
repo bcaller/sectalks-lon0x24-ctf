2a78678bcb7fde9c6e96db0333bc087bb76873e2
bcaller <bcaller@users.noreply.github.com>
2019-10-23 23:37:05 +0100

    Upload kms flag and associated flag

--- upload.sh
+++ upload.sh
@@ -5,3 +5,5 @@ aws s3 cp ctf.zip s3://bc-ctf/ctf.zip
 aws s3 cp hangul/flag.enc s3://bc-ctf/hangul-flag.enc --content-type text/plain --acl public-read
 aws s3 cp short-flag-zip/short-flag.zip s3://bc-ctf/short-flag.enc --acl public-read
 aws s3 cp wasm/wasm.zip s3://bc-ctf/wasm.zip --acl public-read
+aws s3 cp kms/flag.enc s3://bc-ctf/zeppelin-ctf-flag-here/flag-ciphertext.enc
+aws s3 cp kms/grant.txt s3://bc-ctf/zeppelin-ctf-flag-here/grant.txt
