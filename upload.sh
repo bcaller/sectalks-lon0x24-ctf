2f5d1a468ec169153fc1a6f8e91a11b73a75b848
bcaller <bcaller@users.noreply.github.com>
2019-10-24 02:33:52 +0100

    Stupid navigator battery level challenge

--- upload.sh
+++ upload.sh
@@ -2,8 +2,9 @@
 git archive -o ctf.zip @
 aws s3 cp ctf.zip s3://bc-ctf/ctf.zip
 
-aws s3 cp hangul/flag.enc s3://bc-ctf/hangul-flag.enc --content-type text/plain --acl public-read
+aws s3 cp hangul/flag.enc s3://bc-ctf/hangul-flag.enc --acl public-read
 aws s3 cp short-flag-zip/short-flag.zip s3://bc-ctf/short-flag.enc --acl public-read
 aws s3 cp wasm/wasm.zip s3://bc-ctf/wasm.zip --acl public-read
 aws s3 cp kms/flag.enc s3://bc-ctf/zeppelin-ctf-flag-here/flag-ciphertext.enc
 aws s3 cp kms/grant.txt s3://bc-ctf/zeppelin-ctf-flag-here/grant.txt
+aws s3 cp battery/index.html s3://bc-ctf/power.html --acl public-read
