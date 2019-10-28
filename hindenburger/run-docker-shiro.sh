9e1569a4a440ebd0a6ed8fcb24b54543aefc29b8
bcaller <bcaller@users.noreply.github.com>
2019-10-28 00:56:00 +0000

    fix run-docker-shiro.sh

--- hindenburger/run-docker-shiro.sh
+++ hindenburger/run-docker-shiro.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -u
-NAME="$(basename $(pwd))-shiro"
+BASENAME="$(basename $(pwd))"
+NAME="${BASENAME}-shiro"
 
 echo "$SHIRO_FLAG" > /dev/null
 
 docker stop $NAME
 docker rm $NAME
 
+docker build . -t $BASENAME
 docker build -f Dockerfile.shiro . -t $NAME
 docker create \
   -p "${1:-8001}:8080" \
